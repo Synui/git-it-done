34eabde3ca5ef3b5cc4789ff4052405b75d0e0ff
Synui <jonathon.schoenholz@yahoo.com>
2021-12-08 23:41:40 -0500

    added featured languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+var languageButtonsEl = document.querySelector("#language-buttons")
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
@@ -27,6 +28,36 @@ var getUserRepos = function(user) {
             console.log(error);
         });
 }
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+
+    if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+      }
+}
+
+// language parameter function for featured repositories
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                // convert to JSON
+                response.json()
+                    .then(function(data) {
+                    displayRepos(data.items, language);
+            });
+            } else {
+            alert('Error: ' + response.statusText);
+            }
+        });
+  };
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -99,3 +130,5 @@ var displayRepos = function(repos, searchTerm) {
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
 
+// event listener for language parameter buttons
+languageButtonsEl.addEventListener("click", buttonClickHandler);
